0995121b15f92381f308a6decfeffc51b0401640
tbkong2 <144826371+tbkong2@users.noreply.github.com>
2026-09-07 14:43:32 -0600

    revert changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -63,7 +63,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'center',
     flexDirection: 'row',
   },
